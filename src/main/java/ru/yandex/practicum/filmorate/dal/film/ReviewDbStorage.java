fd53af0f95f0e3b7c94286434c8a39be8a262a0c
Valerii <Kovaleff615@yandex.ru>
2024-08-22 21:07:01 +0300

    изменил метод расчета количества лайков на отзывах, для лучшей работы базы данных

--- src/main/java/ru/yandex/practicum/filmorate/dal/film/ReviewDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/film/ReviewDbStorage.java
@@ -2,40 +2,72 @@ package ru.yandex.practicum.filmorate.dal.film;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Review;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class ReviewDbStorage extends BaseDbStorage<Review> {
-    private static final String INSERT_QUERY = "INSERT INTO reviews(content, is_positive, user_id, film_id) VALUES(?, ?, ?, ?)";
-    private static final String UPDATE_QUERY = "UPDATE reviews SET content = ?, is_positive = ?, user_id = ?, film_id = ? WHERE review_id = ?";
-    private static final String FIND_BY_ID_QUERY = "SELECT * FROM reviews WHERE review_id = ?";
-    private static final String DELETE_BY_ID = "DELETE FROM reviews WHERE review_id = ?";
-    private static final String FIND_ALL_QUERY = " SELECT * FROM reviews LIMIT ?";
-    private static final String FIND_BY_ID_FILM_QUERY = " SELECT * FROM reviews WHERE film_id = ? LIMIT ?";
-    private static final String INSERT_LIKE_QUERY = " INSERT INTO useful(review_id, user_id, useful) VALUES(?, ?, ?)";
-    private static final String DELETE_LIKE_BY_ID = "DELETE FROM useful WHERE review_id = ? AND user_id = ? AND useful = ?";
-    private static final String DELETE_LIKE_BY_ID_NO_USEFUL = "DELETE FROM useful WHERE review_id = ? AND user_id = ?";
-    private static final String FIND_LIKE_COUNT_QUERY = " SELECT * FROM useful WHERE review_id = ? AND useful = true";
-    private static final String FIND_DISLIKE_COUNT_QUERY = "SELECT * FROM useful WHERE review_id = ? AND useful = false";
+    private static final String INSERT_QUERY = """
+            INSERT INTO reviews(content, is_positive, user_id, film_id) VALUES(?, ?, ?, ?)
+            """;
+    private static final String UPDATE_QUERY = """
+            UPDATE reviews SET content = ?, is_positive = ?, user_id = ?, film_id = ? WHERE review_id = ?
+            """;
+    private static final String FIND_BY_ID_QUERY = """
+            SELECT * FROM reviews WHERE review_id = ?
+            """;
+    private static final String DELETE_BY_ID = """
+            DELETE FROM reviews WHERE review_id = ?
+            """;
+    private static final String FIND_ALL_QUERY = """
+            SELECT * FROM reviews LIMIT ?
+            """;
+    private static final String FIND_BY_ID_FILM_QUERY = """
+            SELECT * FROM reviews WHERE film_id = ? LIMIT ?
+            """;
+    private static final String INSERT_LIKE_QUERY = """
+            INSERT INTO useful(review_id, user_id, useful) VALUES(?, ?, ?)
+            """;
+    private static final String DELETE_LIKE_BY_ID = """
+            DELETE FROM useful WHERE review_id = ? AND user_id = ? AND useful = ?
+            """;
+    private static final String DELETE_LIKE_BY_ID_NO_USEFUL = """
+            DELETE FROM useful WHERE review_id = ? AND user_id = ?
+            """;
+    private static final String FIND_LIKE_COUNT_QUERY = """
+            SELECT review_id FROM useful WHERE review_id = ? AND useful = true
+            """;
+    private static final String FIND_DISLIKE_COUNT_QUERY = """
+            SELECT review_id FROM useful WHERE review_id = ? AND useful = false
+            """;
 
     public ReviewDbStorage(JdbcTemplate jdbc, RowMapper<Review> mapper) {
         super(jdbc, mapper);
     }
 
     public Review create(Review review) {
-        long reviewId = insert(INSERT_QUERY, review.getContent(), review.getIsPositive(), review.getUserId(), review.getFilmId());
+        long reviewId = insert(INSERT_QUERY,
+                review.getContent(),
+                review.getIsPositive(),
+                review.getUserId(),
+                review.getFilmId());
         review.setReviewId(reviewId);
         review.setUseful(0);
         return review;
     }
 
     public Review update(Review review) {
-        update(UPDATE_QUERY, review.getContent(), review.getIsPositive(), review.getUserId(), review.getFilmId(), review.getReviewId());
+        update(UPDATE_QUERY,
+                review.getContent(),
+                review.getIsPositive(),
+                review.getUserId(),
+                review.getFilmId(),
+                review.getReviewId());
         return review;
     }
 
@@ -65,16 +97,8 @@ public class ReviewDbStorage extends BaseDbStorage<Review> {
     }
 
     public int findUsefulCount(Long reviewId) {
-        SqlRowSet sqlRowSetLike = jdbc.queryForRowSet(FIND_LIKE_COUNT_QUERY, reviewId);
-        int likeCount = 0;
-        while (sqlRowSetLike.next()) {
-            likeCount++;
-        }
-        SqlRowSet sqlRowSetDislike = jdbc.queryForRowSet(FIND_DISLIKE_COUNT_QUERY, reviewId);
-        int dislikeCount = 0;
-        while (sqlRowSetDislike.next()) {
-            dislikeCount++;
-        }
-        return likeCount - dislikeCount;
+        List<Long> likeList = new ArrayList<>(findManyInstances(FIND_LIKE_COUNT_QUERY, Long.class, reviewId));
+        List<Long> dislikeList = new ArrayList<>(findManyInstances(FIND_DISLIKE_COUNT_QUERY, Long.class, reviewId));
+        return likeList.size() - dislikeList.size();
     }
 }
